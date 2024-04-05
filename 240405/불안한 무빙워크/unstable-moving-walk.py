1496a2e7c30549ea1d80ac76fda5b241d2009c8d
Taeseok LEE <106011008+TaeSeokL@users.noreply.github.com>
2024-04-05 23:30:53 +0900

    Title: 불안한 무빙워크, Time: 407ms, Memory: 26MB, Status: Passed - CodeTree

--- 240405/불안한 무빙워크/unstable-moving-walk.py
+++ 240405/불안한 무빙워크/unstable-moving-walk.py
@@ -1,3 +1,4 @@
+from collections import deque
 if __name__=='__main__':
     n, k = map(int,input().split())                 # n 길이 / k 종료조건
     moving_walk = list(map(int,input().split()))    # 무빙워크
@@ -21,12 +22,13 @@ if __name__=='__main__':
             ori_pos = person[i]
             new_pos = ori_pos + 1
 
-            if new_pos == n-1:                                          # 내리는 자리 일때
-                person.pop(i)
-                moving_walk[new_pos] -= 1
-            elif new_pos not in person and moving_walk[new_pos] > 0:    # 다음칸에 안전성이 충분하고, 사람이 없을때
-                person[i] = new_pos
-                moving_walk[new_pos] -= 1
+            if moving_walk[new_pos] > 0:
+                if new_pos == n-1:                                          # 내리는 자리 일때
+                    person.pop(i)
+                    moving_walk[new_pos] -= 1
+                elif new_pos not in person :    # 다음칸에 안전성이 충분하고, 사람이 없을때
+                    person[i] = new_pos
+                    moving_walk[new_pos] -= 1
 
         # 1번칸에 사람 없고 안전성이 0이상이면 사람 올리기
         if 0 not in person and moving_walk[0] > 0:
